e1e5436d0c8b55360ac17664e49537e26a41bfd9
Aman23069 <58472535+Aman23069@users.noreply.github.com>
2023-07-21 21:08:39 +0530

    Testing Router

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import { StyleSheet, Text, View,StatusBar,Image,AppRegistry } from 'react-native';
 import Inputs from './Input.js';
+//import Routes from './Routes.js'
+import WebViewExample from './web_view_example.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
     <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/>
-      <Inputs/>
+     // <Inputs/>
+      <WebViewExample/>
+     // <Image source = {require("./Images/success.png")}/>
+     <Text> "Aman"</Text>
       <StatusBar style="auto" />
     </View>
   );
