a704507d0604c31fba0dd93e4705db25340f206d
Aman23069 <58472535+Aman23069@users.noreply.github.com>
2023-07-21 17:11:22 +0530

    change App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import Inputs from './Input.js';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/>
+      <Inputs/>
       <StatusBar style="auto" />
     </View>
   );
